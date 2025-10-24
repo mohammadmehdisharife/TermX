0c76429ef6f1712f2f11565309d846d074879cd8
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-10-24 17:27:31 +0330

    system programing for cat.c

--- src/cat.c
+++ src/cat.c
@@ -1,20 +1,38 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc,char *argv[]) {
-	if (argc != 2){
-		fprintf(stderr, "Usage: cat <file_name>\n");
-		return 0;
-	}
-	FILE *file;
-	int c;
-	file = fopen(argv[1],"r");
-	if (file == NULL){
-		perror("cannot open file");
-		return 1;
-	}
-	while((c = fgetc(file)) != EOF)
-		putchar(c);
-	
-	fclose(file);
-	return 0;
+#define BUF_SIZE 4096
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+	  fprintf(stderr, "Using: program [filename]\n");
+	  return 1;
+  }
+
+  int fd = open(argv[1], O_RDONLY);
+  if (fd < 0) {
+	  perror("open file failed");
+	  return 1;
+  }
+
+  ssize_t bytes_read, bytes_write;
+  char buffer[BUF_SIZE];
+
+  while ((bytes_read = read(fd, buffer, BUF_SIZE)) > 0) {
+	  bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
+	  if (bytes_read != bytes_write) {
+		  perror("write file failed");
+		  return 1;
+	  }
+  }
+
+  if (bytes_read < 0) {
+    perror("read failed");
+    return 1;
+  }
+
+  close(fd);
+  return 0;
 }
